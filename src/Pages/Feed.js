d4d1572e432d9c3bff00b1e81788c204e675df85
Harshit Shukla <harshit.s@houseofweb3.com>
2024-02-17 12:06:41 +0530

    restructing the code in feed.js

--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -44,136 +44,143 @@ const Feed = () => {
         }}
       >
         <div
-          className=""
           style={{
             width: "70%",
             padding: "0px 10px 0px 22px",
-            gap: "10px",
             display: "flex",
             flexDirection: "column",
             overflowY: "auto",
             height: "100%",
           }}
         >
-          <div
-            className=""
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "10px ",
-              height: "100%",
-            }}
-          >
-            <label>Recent Predictions</label>
-            <a href="">View All</a>
-          </div>
-          {cards.map((card, index) => (
-            <div className="feed-card" key={index}>
-              <div className="card-header">
-                <div
-                  className=""
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <div
-                    className="card-img"
-                    style={{
-                      borderRadius: "50%",
-                      border: "1px solid #4B6CC2",
-                      padding: "2px",
-                      height: "fit-content",
-                      width: "fit-content",
-                    }}
-                  >
-                    <Image src="/feed-profile.svg" alt="profile-image" />
-                  </div>
-                  <div
-                    className="profile-name"
-                    style={{
-                      paddingLeft: "10px",
-                      flexDirection: "column",
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <h5>{card.name}</h5>
-                    <span>Economy</span>
-                  </div>
-                </div>
-                <button
-                  style={{
-                    backgroundColor: "#4B6CC2",
-                    border: "none",
-                    borderRadius: "25px",
-                    fontSize: "16px",
-                    padding: "12px 36px",
-                    color: "#fff",
-                  }}
-                >
-                  Proof
-                </button>
-              </div>
-              <div className="card-status">
-                <Image src="/status.svg" />
-                <p>
-                  Status : <span> Pending</span>
-                </p>
-              </div>
-              <div className="card-dates">
-                <p>
-                  Made on : <span>2023/2/4</span>
-                </p>
-                <p>
-                  Resolves on : <span>2023/2/4</span>
-                </p>
-              </div>
-              <div className="card-description">
-                <p>
-                  Elon Musk predicts that Tesla will solve self-driving and
-                  surpass the average person's accident probability by
-                  2023dvksnjs k <a href="">Read More</a>
-                </p>
-              </div>
-              <div className="card-image">
-                <Image src={card.src} />
-              </div>
-              <div className="card-footer">
-                <div className="likes">
-                  <ImArrowUp
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  />
-                  <span>30</span>
-                </div>
-                <div className="dislikes">
-                  <ImArrowDown
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  />
-                  <span>13</span>
-                </div>
-                <div className="comment">
-                  <IoChatbubbleEllipses
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  />
-                </div>
-                <div className="share">
-                  <RiSendPlaneFill
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  />
-                </div>
-              </div>
+          <div className="recent-predictions">
+            <div
+              className=""
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "10px ",
+              }}
+            >
+              <label>Recent Predictions</label>
+              <a href="">View All</a>
             </div>
-          ))}
-
+            <div
+              className="rp-cards"
+              style={{
+                display: "flex",
+                gap: "10px",
+                flexDirection: "column",
+              }}
+            >
+              {cards.map((card, index) => (
+                <div className="feed-card" key={index}>
+                  <div className="card-header">
+                    <div
+                      className=""
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div
+                        className="card-img"
+                        style={{
+                          borderRadius: "50%",
+                          border: "1px solid #4B6CC2",
+                          padding: "2px",
+                          height: "fit-content",
+                          width: "fit-content",
+                        }}
+                      >
+                        <Image src="/feed-profile.svg" alt="profile-image" />
+                      </div>
+                      <div
+                        className="profile-name"
+                        style={{
+                          paddingLeft: "10px",
+                          flexDirection: "column",
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <h5>{card.name}</h5>
+                        <span>Economy</span>
+                      </div>
+                    </div>
+                    <button
+                      style={{
+                        backgroundColor: "#4B6CC2",
+                        border: "none",
+                        borderRadius: "25px",
+                        fontSize: "16px",
+                        padding: "12px 36px",
+                        color: "#fff",
+                      }}
+                    >
+                      Proof
+                    </button>
+                  </div>
+                  <div className="card-status">
+                    <Image src="/status.svg" />
+                    <p>
+                      Status : <span> Pending</span>
+                    </p>
+                  </div>
+                  <div className="card-dates">
+                    <p>
+                      Made on : <span>2023/2/4</span>
+                    </p>
+                    <p>
+                      Resolves on : <span>2023/2/4</span>
+                    </p>
+                  </div>
+                  <div className="card-description">
+                    <p>
+                      Elon Musk predicts that Tesla will solve self-driving and
+                      surpass the average person's accident probability by
+                      2023dvksnjs k <a href="">Read More</a>
+                    </p>
+                  </div>
+                  <div className="card-image">
+                    <Image src={card.src} />
+                  </div>
+                  <div className="card-footer">
+                    <div className="likes">
+                      <ImArrowUp
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      />
+                      <span>30</span>
+                    </div>
+                    <div className="dislikes">
+                      <ImArrowDown
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      />
+                      <span>13</span>
+                    </div>
+                    <div className="comment">
+                      <IoChatbubbleEllipses
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                    <div className="share">
+                      <RiSendPlaneFill
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
           <div className="top-predictors">
             <div
               className=""
